b344e2f54ed0bf4f2324737c3203d5456961b581
Ismael Victor <ivictormc29@gmail.com>
2026-03-18 17:15:03 +0000

    primeiro exercicio

---
+++ media.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){ 
+    int nota1;
+    int nota2;
+    int nota3;
+    int total;
+        scanf("%d", &nota1);
+        scanf("%d", &nota2);
+        scanf("%d", &nota3);
+        total = (nota1 + nota2 + nota3);
+        printf("Pontos Totais do Ano: \n%d",total);
+    
+       return 0;
+
+}
